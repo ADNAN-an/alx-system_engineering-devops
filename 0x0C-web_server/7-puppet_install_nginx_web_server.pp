be82c8cf2456f1c5f23fe914e7ddd48b5c059ddd
ADNAN <adnan.anjar2002@gmail.com>
2023-12-10 09:05:17 -0800

    push

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,22 +1,18 @@
-# File:   7-puppet_install_nginx_web_server.pp
-# Author: Alex Orland Arévalo Tribaldos
-# email:  <3915@holbertonschool.com>
-
-# Using Puppet| Install Nginx server, setup and configuration
+# Automating project requirements using Puppet
 
 package { 'nginx':
-  ensure => 'installed'
+  ensure => installed,
+}
+
+file_line { 'install':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-enabled/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.github.com/ADNAN-an permanent;',
 }
 
 file { '/var/www/html/index.html':
-  content => 'Hello World',
-}
-
-file_line { 'redirection-301':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  content => 'Hello World!',
 }
 
 service { 'nginx':
